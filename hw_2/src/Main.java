9e080d5bd278192fb2233996a4f5bff88f4b9b5c
egeoruc <ege.oruc@ozu.edu.tr>
2023-12-02 17:50:49 +0300

    functions are no longer static

--- hw_2/src/Main.java
+++ hw_2/src/Main.java
@@ -1,52 +1,70 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class Main {
+
+
+    private Connection connection;
+
+    public Main(String url, String username, String password) {
+        try {
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the HW_2 database.");
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to close the connection.");
+            e.printStackTrace();
+        }
+    }
+
+    public int addSeller(String name, String address) {
+        int createdSeller = -1;
+        try {
+            String query = "INSERT INTO User (name, type, address) VALUES ('" + name + "', 'Seller', '" + address + "');";
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    createdSeller = generatedKeys.getInt(1);
+                    System.out.println("New Seller ID(inside the function): " + createdSeller);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Failed to add seller.");
+            e.printStackTrace();
+        }
+        return createdSeller;
+    }
+
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/HW_2";
         String username = "root";
         String password = "26052708";
 
-       
-        try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            if (connection != null){
-               System.out.println("Connected to the database!");
-               User user = new User(3, "Ege", "Seller", "Dorm 6");
-               addUser(connection, user);
-            }
-            else{
-                System.out.println("Failed to make connection!");
-            }
-            
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to the database.");
-            e.printStackTrace();
+        Main main = new Main(url, username, password);
+        if (main.connection != null) {
+            System.out.println("Connected to the database!");
+
+            int createdSeller = main.addSeller("Ali", "Dormitory 2");
+            System.out.println("New Seller ID (Return of Function): " + createdSeller);
+
+            main.closeConnection();
+        } else {
+            System.out.println("Failed to make connection!");
         }
     }
-    
-    
-    private static void addUser(Connection connection, User user) {
-        try {
-
-            String query = "INSERT INTO User VALUES (" + user.getUserId() + ", '" + user.getName() + "', '" + user.getType() + "', '" + user.getAddress() + "');";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(query);
-
-            } 
-
-        } catch (SQLException e){
-                System.out.println("Failed to add user.");
-                e.printStackTrace();
-            }
-    }
 }
-
-    
-    
-
-
